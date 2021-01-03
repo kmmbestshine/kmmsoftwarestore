dd6a045fd550a54ccaa61962a2d1e4817c30cbe0
example1 <32572574+bestshine@users.noreply.github.com>
2021-01-03 22:37:06 +0530

    initial commit
    initial commit

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,6 +3,8 @@
         Powered by <a href="{{ url('https://github.com/nafiesl/free-pmo') }}" target="_blank">Bestshine</a>,
         and developed with pleasure by the
         <a href="{{ url('https://github.com/nafiesl/free-pmo/graphs/contributors') }}" target="_blank">Kumaravel</a>.
+         For New Register
+        <a href="{{url('franchasee/registerform')}}" class="btn btn-info btn-rounded">New Franchisee<span class="new-gif-wr"></span> </a>
     </div>
     {{-- Free PMO is an opensource software you are free to remove this footer text or section. --}}
 </footer>
